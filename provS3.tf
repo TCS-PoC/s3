e668a85d7d4704a37095059d5da8c381d1cbbdd3
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-16 19:33:16 +0530

    Update provS3.tf

--- provS3.tf
+++ provS3.tf
@@ -1,9 +1,14 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
-  acl    = "private"
+  acl = "private"
 
   tags {
-    Name        = "My bucket"
+    Name = "My bucket"
     Environment = "Dev"
   }
 }
